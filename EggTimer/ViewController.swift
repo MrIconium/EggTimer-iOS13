eb98210a1653447a02d990d19eb9e498c2b8d1e6
Mehmet Emrah Konya <mehmetemrahk@gmail.com>
2024-05-21 19:35:40 +0300

    Add timer functionality

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,29 +14,58 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var eggTimer: UIProgressView!
     
+    var cookingTime: Float = 0.0
+    let cookingTimes = ["Soft":3, "Medium":5, "Hard":7]
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         eggTimer.progress = 0
+        
     }
+    
+  
     
     @IBAction func eggSelected(_ sender: UIButton) {
         if let buttonTitle = sender.currentTitle {
-            
+            if let timerPressed = timer {
+                timerPressed.invalidate()
+            }
             switch buttonTitle {
             case "Soft":
                 print("Soft is selected")
-                eggTimer.progress = 0.3
+                cookingTime = 3.0
             case "Medium":
                 print("Medium is selected")
-                eggTimer.progress = 0.6
+                cookingTime = 5.0
             case "Hard":
                 print("Hard is selected")
-                eggTimer.progress = 0.9
+                cookingTime = 7.0
             default:
                 print("This shouldn't happen")
             }
+            
+            print(cookingTimes[buttonTitle]!)
+            startTimer()
+            eggTimer.progress = 0
         }
-       
     }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter(){
+        if cookingTime > 0 {
+            var iterationTime = 1/cookingTime
+            cookingTime -= 1
+            print(cookingTime)
+            eggTimer.progress += iterationTime
+        } else {
+            timer?.invalidate()
+            eggTimer.progress = 1
+        }
+    }
+
 }
