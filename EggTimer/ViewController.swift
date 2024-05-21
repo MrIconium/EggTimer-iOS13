e45983729ef0b9db1116453e4a1c9667b9730f66
Mehmet Emrah Konya <mehmetemrahk@gmail.com>
2024-05-21 15:08:54 +0300

    Add switch cases for the egg types and add progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+   
+    
+    @IBOutlet weak var eggTimer: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        eggTimer.progress = 0
+    }
+    
+    @IBAction func eggSelected(_ sender: UIButton) {
+        if let buttonTitle = sender.currentTitle {
+            
+            switch buttonTitle {
+            case "Soft":
+                print("Soft is selected")
+                eggTimer.progress = 0.3
+            case "Medium":
+                print("Medium is selected")
+                eggTimer.progress = 0.6
+            case "Hard":
+                print("Hard is selected")
+                eggTimer.progress = 0.9
+            default:
+                print("This shouldn't happen")
+            }
+        }
+       
+    }
 }
